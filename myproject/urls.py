1ac0fbe6bc36b86ce4711541af4f4d7c029140ec
최재혁 <wogur247@hanyang.ac.kr>
2020-04-20 00:16:24 +0900

    blogproject+static+media

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+import portfolio.views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
     path('blog/', include('blog.urls')),
+    path('portfolio/', include('portfolio.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
