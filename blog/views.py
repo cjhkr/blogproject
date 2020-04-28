a27ccfc01f55b730887cbf0fc00f309211b8234c
최재혁 <wogur247@hanyang.ac.kr>
2020-04-28 22:41:04 +0900

    +pagination

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 # Create your views here.
 
 def home(request):
     blogs = Blog.objects
-    return render(request, 'home.html', {'blogs' : blogs})
+
+    blog_list = Blog.objects.all()
+
+    paginator = Paginator(blog_list,3)
+
+    page = request.GET.get('page')
+
+    posts = paginator.get_page(page)
+
+    return render(request, 'home.html', {'blogs' : blogs, 'posts' : posts})
 
 
 def new(request):
